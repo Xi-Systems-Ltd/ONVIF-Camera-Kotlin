0bfca52d0cd8302bd08b3bb3aa4b8ba758128151
PHernimanXiSystems <pete.herniman@xisystems.co.uk>
2026-01-29 14:42:33 +0000

    Added import for IO dispatcher.

--- onvifcamera/src/commonMain/kotlin/uk/co/xisystems/onvifcamera/network/OnvifDiscoveryManager.kt
+++ onvifcamera/src/commonMain/kotlin/uk/co/xisystems/onvifcamera/network/OnvifDiscoveryManager.kt
@@ -2,6 +2,7 @@ package uk.co.xisystems.onvifcamera.network
 
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
 import kotlinx.coroutines.flow.Flow
 import uk.co.xisystems.onvifcamera.DiscoveredOnvifDevice
 
